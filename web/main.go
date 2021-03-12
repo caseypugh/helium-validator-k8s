b20b9f5fa3acf06389e504d2973e97c0936e8bc5
Casey Pugh <me@caseypugh.com>
2021-03-12 15:07:23 -0500

    hello world

---
+++ web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "💩 Hello World 💩")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+}
+
+func main() {
+	fmt.Println("Go Web App Started on Port 3000")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
+}
