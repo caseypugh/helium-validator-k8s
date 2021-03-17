6917f394f8e50932c9dbb4758b93a4dc74e327cb
Casey Pugh <me@caseypugh.com>
2021-03-17 13:57:32 -0400

    fix loop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ else
 
   # Delete all the validator pods. StatefulSet will automatically recreate them
   validator_count=$(kubectl get pods | grep -c "helium-validator")
-  for ((i = 0 ; i <= $validator_count ; i++)); do
+  for ((i = 0 ; i < $validator_count ; i++)); do
     kubectl delete pod helium-validator-$i &
   done  
 fi
