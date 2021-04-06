9333460a78ccb1954ad6eb6c891be7f5e1016762
Casey Pugh <me@caseypugh.com>
2021-04-06 10:54:38 -0400

    Minor fixes

--- cg_perf/parse-stats.sh
+++ cg_perf/parse-stats.sh
@@ -6,5 +6,5 @@ rm -f $outfile
 # TODO dynamically parse current validator names
 for i in attractive-vermilion-urchin short-nylon-frog broad-hemp-corgi noisy-iris-loris; do 
   echo; echo $i
-  grep $i cg_perf/* >> cg_perf.csv
+  grep "$i" data/*.csv >> cg_perf.csv
 done
