62a95cb0ec547c20890df268e89c29928f478a72
Casey Pugh <me@caseypugh.com>
2021-04-23 12:15:43 -0400

    Remove this, it's not actually useful. Can get what we want via API

--- cg_perf/stats-loop.sh
+++ cg_perf/stats-loop.sh
@@ -4,18 +4,12 @@ cd "$(dirname "$0")"
 outdir="data"
 mkdir -p "$outdir"
 
-outdir2="data-validators"
-mkdir -p "$outdir2"
-
 while [ 1 ]; do
   when=$(date +"%Y-%m-%d-%H_%M")
   outfile="$outdir/$when.csv"
   echo $outfile
-  outfile2="$outdir2/validators-$when.json"
-  echo $outfile2
 
   ../scripts/validator cg_perf --format=csv > $outfile
-  curl -s https://testnet-api.helium.wtf/v1/validators > $outfile2
 
   sleep 60
 done
